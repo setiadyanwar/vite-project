900d8b097440ac18d654b3f0237468d2efd4dca1
unknown <setiadyanwar@gmail.com>
2023-05-28 13:28:02 +0700

    add create todo feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { nanoid } from 'nanoid';
+import "./App.css";
+
+
+const DUMMY_TODO = [
+  {
+    id: nanoid(),
+    title: "Belajar React",
+    isCompleted: false,
+  },
+];
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState(DUMMY_TODO);
+  const [newTodo, setNewTodo] = useState('');
+
+  function addNewTodo() {
+    const updatedTodos = [...todos];
+    const objTodo = {
+      id: nanoid(),
+      title: newTodo,
+      isCompleted: false
+    };
+
+    updatedTodos.push(objTodo);
+    setTodos(updatedTodos);
+    setNewTodo('');
+  }
 
   return (
     <>
-    <h1> Hello Kawan-kawan</h1>
+      <div className="container">
+        <h2>Todo App</h2>
+        <input
+          type="text"
+          placeholder="Enter your todo"
+          value={newTodo}
+          onChange={event => setNewTodo(event.target.value)}
+        />
+        <button onClick={() => addNewTodo()}>Create</button>
+        <ul>
+          {todos.map((todo) => (
+            <li key={todo.id}>{todo.title}</li>
+          ))}
+        </ul>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
